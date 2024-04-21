ed512f3fc00ec31797157a59f7fd65390d9e25a5
devmdfaiz <155801240+devmdfaiz@users.noreply.github.com>
2024-04-21 22:47:08 +0530

    Update layout.tsx

--- src/app/landing-page/layout.tsx
+++ src/app/landing-page/layout.tsx
@@ -1,6 +1,4 @@
 // This Layout page is build for to provide common headers and footers to landing page
-
-import AuthProvider from "@/context/Auth-AuthProvider";
 import ThemeProvider from "@/context/ThemeProvider";
 
 export default async function RootLayout({
@@ -9,10 +7,8 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <AuthProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <div className="">{children}</div>
       </ThemeProvider>
-    </AuthProvider>
   );
 }
